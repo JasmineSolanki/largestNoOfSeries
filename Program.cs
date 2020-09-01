2007dc8f9e882dfb7c7f3cb84eaa1a3f36d61258
Jasmine Solanki <jasminesolanki2018@gmail.com>
2020-09-01 07:59:27 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace largestNoOfSeries
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            int number,large=0;
+
+           while(true)
+           {
+    
+             Console.WriteLine("Enter the number");
+             number = Convert.ToInt32(Console.ReadLine());
+             
+                    if(number>large)
+                    {
+                        large=number;
+                    }     
+            if(number==-1)
+            break;
+           }  
+            Console.WriteLine("The largest number is " +large); 
+        }
+    }
+}
